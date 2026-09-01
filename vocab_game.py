76fb70bc2fe2c6764b576a91dd115d4b7caed189
chomponut <56771@yupparaj.ac.th>
2026-09-01 10:56:25 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -108,7 +108,6 @@ ans4 = st.text_input(
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
-
 st.session_state.ans3_val = ans3
 st.session_state.ans4_val = ans4
 
