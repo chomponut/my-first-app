392140a77361dd9cb505caafae49c87588fae59f
chomponut <56771@yupparaj.ac.th>
2026-09-01 10:49:12 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -28,7 +28,7 @@ def reset_game():
 # 📌 ฟังก์ชัน MessageBox (Dialog)
 # ----------------------------------------------------
 @st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1, ans2):
+def show_result_dialog(ans1, ans2, ans3, ans4):
     st.balloons()
     score = 0
 
